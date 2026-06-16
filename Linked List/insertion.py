76654d605158688d8c755c070ba9a4d1a52b6e9c
Saanchi Jain <jainsanchi2205@gmail.com>
2026-06-17 01:24:54 +0530

    Did all linked list basic operations on singly linked list

--- Linked List/insertion.py
+++ Linked List/insertion.py
@@ -6,7 +6,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-    def insert(self,data):
+    def insert_at_end(self,data):
         new_node = Node(data)
         if self.head == None:
             self.head = new_node
@@ -15,12 +15,45 @@ class LinkedList:
         while temp.next!=None:
             temp =temp.next
         temp.next=new_node
+
+    def insert_at_beg(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        new_node.next= self.head
+        self.head= new_node
+    
+    def insert_at_idx(self,data,idx):
+        if idx==0:
+            self.insert_at_beg()
+        newnode = Node(data)
+        temp = self.head
+        for i in range(idx-1):
+            if temp is None or temp.next is None:
+                print('invalid idx')
+                return
+            temp = temp.next
+        newnode.next = temp.next
+        temp.next=newnode
+
     
     def deletion_from_begining(self):
         if self.head is None:
             print('List is Empty')
             return
         self.head = self.head.next
+
+    def delete_at_idx(self,idx):
+        if idx==0:
+            self.deletion_from_begining()
+            return
+        temp = self.head
+        for i in range(idx-1):
+            if temp is None or temp.next is None:
+                print('index out of range')
+            temp= temp.next
+        temp.next = temp.next.next
 
     def display(self):
         temp = self.head
@@ -29,10 +62,20 @@ class LinkedList:
             temp = temp.next
         print('None')
 
+
+
+
 l1 = LinkedList()
-l1.insert(1)
-l1.insert(2)
-l1.insert(3)
+l1.insert_at_end(1)
+l1.insert_at_end(2)
+l1.insert_at_end(3)
 l1.display()
 l1.deletion_from_begining()
+l1.display()
+l1.insert_at_end(4)
+l1.insert_at_end(5)
+l1.display()
+l1.delete_at_idx(2)
+l1.insert_at_beg(6)
+l1.insert_at_idx(7,2)
 l1.display()
