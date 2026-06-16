a2d4d21ca7d47899a94f95d8a9984ba91dc24960
Saanchi Jain <jainsanchi2205@gmail.com>
2026-06-17 00:42:34 +0530

    Added Deletion at beginning

--- Linked List/insertion.py
+++ Linked List/insertion.py
@@ -16,6 +16,12 @@ class LinkedList:
             temp =temp.next
         temp.next=new_node
     
+    def deletion_from_begining(self):
+        if self.head is None:
+            print('List is Empty')
+            return
+        self.head = self.head.next
+
     def display(self):
         temp = self.head
         while temp!=None:
@@ -27,4 +33,6 @@ l1 = LinkedList()
 l1.insert(1)
 l1.insert(2)
 l1.insert(3)
+l1.display()
+l1.deletion_from_begining()
 l1.display()
