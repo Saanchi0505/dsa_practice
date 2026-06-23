0a8ae4d560e7af8400b7aabe594fc6f83edef3b3
Saanchi Jain <jainsanchi2205@gmail.com>
2026-06-23 19:06:23 +0530

    Implementation of postfix to infix expression

---
+++ Stacks&Queues/postfix_to_infix.py
@@ -0,0 +1,17 @@
+def post_to_in(exp):
+    stack = []
+    i=0
+    for i in range(len(exp)):
+        if (exp[i] >='A' and exp[i]<='Z') or (exp[i]>='a' and exp[i]<='z') or (exp[i]>='0' and exp[i]<='9'):
+            stack.append(exp[i])
+        else:
+            t1 = stack[-1]
+            stack.pop()
+            t2 = stack[-1]
+            stack.pop()
+            ch = '(' + t2  + exp[i] + t1 + ')'
+            stack.append(ch)
+    return stack[-1]
+
+exp = "AB+C*"
+print(post_to_in(exp))
