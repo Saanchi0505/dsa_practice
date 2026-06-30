505f0484bb4a0d0bfcc3b25f5caa8a0607170405
Saanchi Jain <jainsanchi2205@gmail.com>
2026-06-30 14:08:28 +0530

    Asteroids collision

---
+++ Stacks&Queues/asteriods_collision.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        st =[]
+        for i  in range(len(asteroids)):
+            if asteroids[i] >0:
+                st.append(asteroids[i])
+            else:
+                while st and st[-1]>0 and st[-1]<abs(asteroids[i]):
+                    st.pop()
+                if st and st[-1]==abs(asteroids[i]):
+                    st.pop()
+                elif len(st)==0 or st[-1] <0:
+                    st.append(asteroids[i])
+        return st
